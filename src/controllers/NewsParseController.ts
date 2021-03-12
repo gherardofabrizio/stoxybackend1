fdea325755bb225872e58bce411c46438059c575
Sergey Gerasimov <mndm@w2f2.com>
2021-03-12 13:09:15 +0200

    Tweak check for news duplicates logic

--- src/controllers/NewsParseController.ts
+++ src/controllers/NewsParseController.ts
@@ -247,6 +247,7 @@ export default class NewsParseController {
       .where({
         title
       })
+      .andWhere('createdAt', '>', moment().subtract(1, 'month').toDate())
       .first()
     if (checkNews) {
       return
