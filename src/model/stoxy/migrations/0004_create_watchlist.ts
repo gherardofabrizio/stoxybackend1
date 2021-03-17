68dbcf8b4fec953c18fc47c556b3ea4c2c454b29
Sergey Gerasimov <mndm@w2f2.com>
2021-03-17 18:09:03 +0200

    Fix tickerId length at migrations

--- src/model/stoxy/migrations/0004_create_watchlist.ts
+++ src/model/stoxy/migrations/0004_create_watchlist.ts
@@ -5,7 +5,7 @@ export default {
     knex.schema.createTable('watchlist', table => {
       table.increments('id').primary()
       table.integer('profileId').unsigned().references('id').inTable('profiles').onDelete('CASCADE')
-      table.string('tickerId', 16).references('symbol').inTable('tickers').onDelete('CASCADE')
+      table.string('tickerId', 32).references('symbol').inTable('tickers').onDelete('CASCADE')
       table.boolean('isNotificationsEnabled').defaultTo('true')
       table.dateTime('createdAt').notNullable().defaultTo(knex.fn.now())
       table.dateTime('updatedAt').notNullable().defaultTo(knex.fn.now())
