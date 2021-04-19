220b6ccf3d466abaeafc5c57fa93b3b63d1190e8
Sergey Gerasimov <mndm@w2f2.com>
2021-04-19 16:47:44 +0300

    Added more items to builtInNewsSources.ts

--- src/model/seedData/builtInNewsSources.ts
+++ src/model/seedData/builtInNewsSources.ts
@@ -37,5 +37,77 @@ export const builtInNewsSources: Array<{
     title: 'Yahoo Finance',
     siteURL: 'https://finance.yahoo.com/',
     rssFeedURL: 'https://finance.yahoo.com/news/rssindex'
+  },
+  {
+    title: 'PR Newswire',
+    siteURL: 'https://www.prnewswire.com',
+    rssFeedURL: 'https://www.prnewswire.com/rss/news-releases-list.rss'
+  },
+  {
+    title: 'GlobeNewswire',
+    siteURL: 'https://www.globenewswire.com',
+    rssFeedURL:
+      'https://www.globenewswire.com/RssFeed/subjectcode/39-Stock%20Market%20News/feedTitle/GlobeNewswire%20-%20Stock%20Market%20News'
+  },
+  {
+    title: 'Zacks',
+    siteURL: 'https://www.zacks.com',
+    rssFeedURL: 'https://scr.zacks.com/rss/pressrelease.aspx'
+  },
+  {
+    title: 'SmarterAnalyst',
+    siteURL: 'https://www.smarteranalyst.com',
+    rssFeedURL: 'https://www.smarteranalyst.com/feed/'
+  },
+  {
+    title: 'Motley Fool',
+    siteURL: 'https://www.fool.com',
+    rssFeedURL:
+      'https://www.fool.com/a/feeds/foolwatch?format=rss2&id=foolwatch&apikey=foolwatch-feed'
+  },
+  {
+    title: 'GuruFocus.com',
+    siteURL: 'https://www.gurufocus.com',
+    rssFeedURL: 'https://www.gurufocus.com/rss.php'
+  },
+  {
+    title: 'MarketWatch',
+    siteURL: 'https://www.marketwatch.com',
+    rssFeedURL: 'http://feeds.marketwatch.com/marketwatch/topstories/'
+  },
+  {
+    title: 'The Wall Street Journal',
+    siteURL: 'https://www.wsj.com',
+    rssFeedURL: 'https://feeds.a.dj.com/rss/RSSMarketsMain.xml'
+  },
+  {
+    title: 'Seeking Alpha',
+    siteURL: 'https://seekingalpha.com',
+    rssFeedURL: 'https://seekingalpha.com/market_currents.xml'
+  },
+  {
+    title: 'Investopedia',
+    siteURL: 'https://www.investopedia.com',
+    rssFeedURL: 'https://www.investopedia.com/feedbuilder/feed/getfeed?feedName=rss_headline'
+  },
+  {
+    title: 'CNN',
+    siteURL: 'https://cnn.com',
+    rssFeedURL: 'http://rss.cnn.com/rss/money_news_international.rss'
+  },
+  {
+    title: 'Businesswire',
+    siteURL: 'https://www.businesswire.com',
+    rssFeedURL: 'https://feed.businesswire.com/rss/home/?rss=G1QFDERJXkJeEFpQWw=='
+  },
+  {
+    title: 'Exec edge',
+    siteURL: 'https://executives-edge.com',
+    rssFeedURL: 'https://executives-edge.com/feed/'
+  },
+  {
+    title: 'Investing.com',
+    siteURL: 'https://www.investing.com',
+    rssFeedURL: 'https://www.investing.com/rss/news.rss'
   }
 ]
